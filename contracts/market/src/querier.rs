4fdc878add8335113f69a1aed593204f92d78eea
CaverPerson <cavernprotocol@gmail.com>
2023-03-23 11:47:37 +0100

    Revert "Limited the borrow incentives distribution rate"
    This reverts commit ee2ec2381c777b60b3027f5cd346d520c5be8488.

--- contracts/market/src/querier.rs
+++ contracts/market/src/querier.rs
@@ -123,14 +123,11 @@ pub fn query_borrow_reserves_incentives_rate(
 }
 
 pub fn query_target_deposit_rate(deps: Deps, overseer_contract: Addr) -> StdResult<Decimal256> {
-    let overseer_config = query_overseer_config(deps,overseer_contract)?;
+    let overseer_config: ConfigResponse =
+        deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
+            contract_addr: overseer_contract.to_string(),
+            msg: to_binary(&OverseerQueryMsg::Config {})?,
+        }))?;
 
     Ok(overseer_config.target_deposit_rate)
-}
-
-pub fn query_overseer_config(deps: Deps, overseer_contract: Addr) -> StdResult<ConfigResponse> {
-    deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-        contract_addr: overseer_contract.to_string(),
-        msg: to_binary(&OverseerQueryMsg::Config {  })?,
-    }))
 }
